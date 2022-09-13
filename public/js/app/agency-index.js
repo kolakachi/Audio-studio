cac5d91c0713a200f467d920586eeee98f56d8e8
Kolawole <kolakachi@gmail.com>
2022-09-13 20:12:02 +0100

    add agency banners

--- public/js/app/agency-index.js
+++ public/js/app/agency-index.js
@@ -1,19 +1,25 @@
 new Vue({
     el: "#agency-index",
     data : {
+        activeAsset: {
+            title:'',
+            agency_url: '',
+            popup_url: '',
+
+        },
         assets:[
-            {title:"DFY Agency Website"},
-            {title:"Letterhead"},
-            {title:"Invoice"},
-            {title:"Brochure"},
-            {title:"Business Card"},
-            {title:"Powerpoint Proposal"},
-            {title:"Video Presentation"},
-            {title:"Flyer"},
-            {title:"Legal Contract"},
-            {title:"Ad Banner"},
-            {title:"Email Sequence"},
-            {title:"Telemarketing Script"}
+            {title:"DFY Agency Website", agency_url:"/assets/img/AGENCY/DESKTOP.png", popup_url:"/assets/img/POP_UP/DESKTOP_1.png"},
+            {title:"Letterhead", agency_url:"/assets/img/AGENCY/LETTERHEAD.png", popup_url:"/assets/img/POP_UP/LETTERHEAD_2.png"},
+            {title:"Invoice", agency_url:"/assets/img/AGENCY/INVOICE.png", popup_url:"/assets/img/POP_UP/INVOICE.png"},
+            {title:"Brochure", agency_url:"/assets/img/AGENCY/BROCHURE.png", popup_url:"/assets/img/POP_UP/BROCHURE_1.png"},
+            {title:"Business Card", agency_url:"/assets/img/AGENCY/BUSINESS_CARD.png", popup_url:"/assets/img/POP_UP/BUSINESS_CARD_1.png"},
+            {title:"Powerpoint Proposal", agency_url:"/assets/img/AGENCY/POWERPOINT.png", popup_url:"/assets/img/POP_UP/POWERPOINT_1.png"},
+            {title:"Video Presentation", agency_url:"/assets/img/AGENCY/VIDEO_PRESENTATION.png", popup_url:"/assets/img/POP_UP/VIDEO_PRESENTATION_1.png"},
+            {title:"Flyer", agency_url:"/assets/img/AGENCY/FLYER_DESIGN.png", popup_url:"/assets/img/POP_UP/FLYER_1.png"},
+            {title:"Legal Contract", agency_url:"/assets/img/AGENCY/LEGAL_CONTRACT.png", popup_url:"/assets/img/POP_UP/LEGAL_CONTRACT_1.png"},
+            {title:"Ad Banner", agency_url:"/assets/img/AGENCY/ADS_BANNER.png", popup_url:"/assets/img/POP_UP/ADS_BANNER_1.png"},
+            {title:"Email Sequence", agency_url:"/assets/img/AGENCY/EMAIL_SEQUENCE.png", popup_url:"/assets/img/POP_UP/EMAIL_SEQUENCE_1.png"},
+            {title:"Telemarketing Script", agency_url:"/assets/img/AGENCY/TELEMARKETING_SCRIPT.png", popup_url:"/assets/img/POP_UP/TELEMARKETING_SCRIPT_1.png"}
         ]
         
     },
@@ -21,7 +27,9 @@ new Vue({
 
     },
     methods: {
-        
+        setActiveAsset(index){
+            this.activeAsset = this.assets[index];
+        }
 
 
     }
--- resources/views/app/agency/index.blade.php
+++ resources/views/app/agency/index.blade.php
@@ -17,8 +17,8 @@
 
 
     <div class="agency-assets">
-      <a v-for="asset in assets" :key="asset.title" href="#" class="asset-item" data-bs-toggle="modal" data-bs-target="#agencyAssetDetailModal" @click.prevent>
-        <div class="asset-image-wrap"></div>
+      <a v-for="(asset, index) in assets" @click="setActiveAsset(index)" :key="asset.title" href="#" class="asset-item" data-bs-toggle="modal" data-bs-target="#agencyAssetDetailModal" @click.prevent>
+        <div class="asset-image-wrap" style="background-size: contain;" v-bind:style="{'backgroundImage': 'url(' + asset.agency_url + ')'}"></div>
         <div class="asset-header">
           <h4 class="asset-title">@{{ asset.title }}</h4>
           <span class="asset-rating icon"><svg width="125" height="23" viewBox="0 0 125 23" fill="none" xmlns="http://www.w3.org/2000/svg" class=""><path d="M12.5 0L15.3064 8.63729H24.3882L17.0409 13.9754L19.8473 22.6127L12.5 17.2746L5.15271 22.6127L7.95913 13.9754L0.611816 8.63729H9.6936L12.5 0Z" fill="#FFB800"></path><path d="M37.5 0L40.3064 8.63729H49.3882L42.0409 13.9754L44.8473 22.6127L37.5 17.2746L30.1527 22.6127L32.9591 13.9754L25.6118 8.63729H34.6936L37.5 0Z" fill="#FFB800"></path><path d="M62.5 0L65.3064 8.63729H74.3882L67.0409 13.9754L69.8473 22.6127L62.5 17.2746L55.1527 22.6127L57.9591 13.9754L50.6118 8.63729H59.6936L62.5 0Z" fill="#FFB800"></path><path d="M87.5 0L90.3064 8.63729H99.3882L92.0409 13.9754L94.8473 22.6127L87.5 17.2746L80.1527 22.6127L82.9591 13.9754L75.6118 8.63729H84.6936L87.5 0Z" fill="#FFB800"></path><path d="M112.5 0L115.306 8.63729H124.388L117.041 13.9754L119.847 22.6127L112.5 17.2746L105.153 22.6127L107.959 13.9754L100.612 8.63729H109.694L112.5 0Z" fill="#FFB800"></path></svg></span>
@@ -28,46 +28,48 @@
         </div>
       </a>
     </div>
-  </div>
-  <div class="modal fade" id="agencyAssetDetailModal" tabindex="-1" aria-labelledby="agencyAssetDetailModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <button class="btn close-btn" data-bs-dismiss="modal">
-          X
-        </button>
-  
-        <div class="modal-body">
-          <div class="asset-detail-header">
-            <h4 class="asset-detail-heading">Agency Asset Details</h4>
-          </div>
-  
-          <div class="asset-detail-row">
-            <div class="asset-detail-col">
-              <div class="asset-image-wrap"></div>
+
+    <div class="modal fade" id="agencyAssetDetailModal" tabindex="-1" aria-labelledby="agencyAssetDetailModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <button class="btn close-btn" data-bs-dismiss="modal">
+            X
+          </button>
+    
+          <div class="modal-body">
+            <div class="asset-detail-header">
+              <h4 class="asset-detail-heading">Agency Asset Details</h4>
             </div>
-            <div class="asset-detail-col">
-              <div class="asset-file-details">
-                <div class="file-details-label">
-                  File Details
-                </div>
-                <div class="file-details-value">
-                  DFY Agency Website
-                </div>
+    
+            <div class="asset-detail-row">
+              <div class="asset-detail-col">
+                <div class="asset-image-wrap" style="background-size: contain;" v-bind:style="{'backgroundImage': 'url(' + activeAsset.popup_url + ')'}"></div>
               </div>
-              <div class="asset-file-details-buttons">
-                <button class="btn btn-primary px-4">
-                  Preview Online
-                </button>
-                <button class="btn btn-success text-white px-4">
-                  Download
-                </button>
+              <div class="asset-detail-col">
+                <div class="asset-file-details">
+                  <div class="file-details-label">
+                    File Details
+                  </div>
+                  <div class="file-details-value">
+                    @{{activeAsset.title}}
+                  </div>
+                </div>
+                <div class="asset-file-details-buttons">
+                  <button class="btn btn-primary px-4">
+                    Preview Online
+                  </button>
+                  <button class="btn btn-success text-white px-4">
+                    Download
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+</div>
+  
 @endsection
 @section('js')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
