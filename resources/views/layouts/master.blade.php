e7d1c3061afcea29628f6d70ac0238a463ba9391
Kolawole <kolakachi@gmail.com>
2022-11-14 13:45:42 +0100

    installed intercom script

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -106,6 +106,17 @@
 {{-- <script src="{{URL::asset('plugins/bootstrap/js/bootstrap5.min.js')}}?v=1"></script> --}}
 {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script> --}}
 {{-- <script type="text/javascript" src="/assets/js/bundle.js"></script></body> --}}
+<script>
+  window.intercomSettings = {
+    api_base: "https://api-iam.intercom.io",
+    app_id: "sksvfqxn"
+  };
+</script>
+
+<script>
+// We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/sksvfqxn'
+(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/sksvfqxn';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+</script>
 @yield('js')
 
 </html>
