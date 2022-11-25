52d74f1468b0798c172a67c4a61a538019cb7438
Kolawole <kolakachi@gmail.com>
2022-11-25 18:38:57 +0100

    subscription update

--- resources/views/emails/frontend-sub.blade.php
+++ resources/views/emails/frontend-sub.blade.php
@@ -1,15 +1,62 @@
 <html>
 <head></head>
     <body>
-        <p><b>Hi {{ $user->first_name }},</b></p>
-        
-        <p>Please keep this information safe as it contains your username and password.</p>
+        <p>Emmanuel GB here - CEO and Founder of Geebital, the creators of Audiostudio.</p>
 
-        <p><b>Your AudioStudio Membership Info:</b></p>
-        
-        <p>Email: {{ $user->email }}</p>
-        <p>Password: {{ $user->plain_password }}</p>
-        <p>Login URL: <a href="{{ route('login') }}" target='_BLANK'>{{ route('login') }}</a></p>
+
+
+<p>I want to personally say a BIG Congratulations to you as you've made the right decision to
+check out Audiostudio, and the entire team is super excited to have you onboard.</p>
+
+
+<p>It’s time to supercharge your marketing and 10X your conversion using Audiostudio.</p>
+
+
+<p>Here are your Audiostudio members area login credentials:</p>
+
+
+<p>Members Area: https://app.audiostudio.cc/login</p>
+
+<p>Username: {{ $user->email }}</p>
+
+<p>Password: {{ $user->plain_password }}</p>
+
+
+
+<p>Need help?</p>
+
+<p>We've created a couple of articles and videos to guide you on how to use Audiostudio,
+and you can find them in the Knowledgebase Section.</p>
+
+
+<p>This is not to say that Audiostudio requires any learning curve. As a matter of fact, Audiostudio
+is the easiest Content & Text-To-Speech software on the internet...</p>
+
+
+<p>I know you're probably seeing that for yourself.</p>
+
+
+<p>But If you encounter any challenge, feel free to contact our support team, and we'll be
+glad to help you out, not as a customer but as a member of our community.</p>
+
+
+<p>Support Desk or email us at support@audiostudio.cc</p>
+
+
+<p>To stay connected and to always receive our mail in your inbox, we'll advise you to
+whitelist our emails (hello@audiostudio.cc and support@audiostudio.cc)</p>
+
+
+
+<p>Click Here to see how you can whitelist our emails in less than 30 seconds.</p>
+
+
+
+<p>Once again, thank you for checking out Audiostudio.</p>
+
+
+<p>Emmanuel GB</p>
+<p>Co-founder & CEO of Audiostudio.</p>
         
     
     </body>
